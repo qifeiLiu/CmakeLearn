d7b2aef2b700009194ca6fe3bf470d1505a848ec
qifeiL <qifei.liu@foxmail.com>
2019-04-21 23:24:00 -0400

    Demo4: configuration file

--- build/math/CMakeFiles/MathFunctions.dir/flags.make
+++ build/math/CMakeFiles/MathFunctions.dir/flags.make
@@ -6,5 +6,5 @@ CXX_FLAGS =
 
 CXX_DEFINES = 
 
-CXX_INCLUDES = 
+CXX_INCLUDES = -I/root/CmakeLearn/math 
 
--- main.cc
+++ main.cc
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "math/MathFunctions.h"
+#include "config.h"
 
+#ifdef USE_MYMATH
+    #include "math/MathFunctions.h"
+#else
+    #include <math.h>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +16,13 @@ int main(int argc, char *argv[])
   }
   double base = atof(argv[1]);
   int exponent = atoi(argv[2]);
+#ifdef USE_MYMATH
+  printf("Now we use our own Math Library.\n");
   double result = power(base,exponent);
+#else
+  printf("Now we use the standard library.\n");
+  double result = pow(base,exponent);
+#endif
   printf("%g ^ %d is %g\n", base, exponent, result);
   return 0;
 }
